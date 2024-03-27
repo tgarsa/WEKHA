28d4323ae346727bad1c118ffe846eb0b2285d7c
Tomas Garcia <tgarsa@me.com>
2024-03-27 23:58:52 +0100

    Adding security to the API.

--- database_access_api/app.py
+++ database_access_api/app.py
@@ -1,23 +1,27 @@
-# In this package, we only define the end points.
-
+# Here only define the end points.
 import numpy as np
 from fastapi import FastAPI
 from pydantic import BaseModel
 from pandas import read_json
 from io import StringIO
 
+# To add security
+from datetime import timedelta
+import security as sec
+from typing_extensions import Annotated
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+# from typing import Any, Union
+
 # importing the methods to communicate with the database
 import players.players as players
 import miscelanea.miscelanea as hosts
 
-from psycopg2.extensions import register_adapter, AsIs
-register_adapter(np.int64, AsIs)
 
-
-# To be sure that the customer send the data in teh correct format.
-# I will need to add more styles.
+# To be sure that the customer send the data in the correct format.
 class Input2(BaseModel):
     df_json: str
+
 
 class Input(BaseModel):
     df_json: str
@@ -26,49 +30,73 @@ class Input(BaseModel):
 
 # Defining our API
 app = FastAPI(title="Database Access",
-              description="A simple api to connect with the database to have the WEHKA data.",
+              description="A simple api to connect with the database to obtain the WEKHA data.",
               version="0.1")
 
 
+# Security
+@app.post("/token", response_model=sec.Token)
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user = sec.authenticate_user(sec.fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=sec.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = sec.create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
 # Access to the players tables.
-@app.post('/new_player', tags=["LoadPlayers"])
-async def new_player(incoming_data: Input2):
+@app.post('/new_player',
+          tags=["LoadPlayers"]
+          )
+async def new_player(incoming_data: Input2,
+                     current_user: Annotated[sec.User, Depends(sec.get_current_active_user)]
+                     ):
     df = read_json(StringIO(incoming_data.df_json))
     result = players.add(df)
     return result
 
 
-@app.post('/update_player', tags=["UpdatePlayer"])
-async def update_player(incoming_data: Input2):
+@app.post('/update_player',
+          tags=["UpdatePlayer"]
+          )
+async def update_player(incoming_data: Input2,
+                        current_user: Annotated[sec.User, Depends(sec.get_current_active_user)]
+                        ):
     df = read_json(StringIO(incoming_data.df_json))
     result = players.update(df)
     return result
 
 
-# @app.get('/get_player', tags=["GetPlayer"])
-# async def get_player(incoming_data: Input):
-#     df = read_json(StringIO(incoming_data.df_json))
-#     result = players.get(df)
-#     return result
-
-
 # Miscelanea Access to the tables.
 @app.post('/new')
-async def new(incoming_data: Input):
+async def new(incoming_data: Input,
+              current_user: Annotated[sec.User, Depends(sec.get_current_active_user)]
+              ):
     df = read_json(StringIO(incoming_data.df_json))
     result = hosts.add(df, incoming_data.table)
     return result
 
 
 @app.post('/update')
-async def update(incoming_data: Input):
+async def update(incoming_data: Input,
+                 current_user: Annotated[sec.User, Depends(sec.get_current_active_user)]
+                 ):
     df = read_json(StringIO(incoming_data.df_json))
     result = hosts.update(df, incoming_data.table)
     return result
 
 
 @app.get('/get')
-async def get(incoming_data: Input):
+async def get(incoming_data: Input,
+              current_user: Annotated[sec.User, Depends(sec.get_current_active_user)]
+              ):
     df = read_json(StringIO(incoming_data.df_json))
     result = hosts.get(df, incoming_data.table)
     return result
---
+++ database_access_api/security.py
@@ -0,0 +1,125 @@
+from datetime import datetime, timedelta
+from pydantic import BaseModel
+
+# To add security
+from typing import Union
+from typing_extensions import Annotated
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status
+# TO use JWT
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+
+
+
+
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "5ef7c476adf87e1a922426133a9fc1dad0706334bc614bec2ba848aba3f0a026"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 300
+
+# Fake database
+fake_users_db = {
+    "tgarsa": {
+        "username": "tgarsa",
+        "full_name": "Tomas Garcia Saiz",
+        "email": "tgarsa@me.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": True,
+    },
+}
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Union[str, None] = None
+
+
+class User(BaseModel):
+    username: str
+    email: Union[str, None] = None
+    full_name: Union[str, None] = None
+    disabled: Union[bool, None] = None
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+# Token
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(current_user: Annotated[User, Depends(get_current_user)]):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
