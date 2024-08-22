ff6a54f37590b2bdc40e28ee15e241920fc8c4d0
Tomas Garcia <tgarsa@me.com>
2024-08-22 18:57:42 +0200

    Test 8

--- database_access_api/templates/acciones_new.js
+++ database_access_api/templates/acciones_new.js
@@ -1,18 +1,12 @@
-// Define a new URL
-
-url = "https://www.as.com"
-// url = "www.as.com"
 
 document.write("<div id=\"acctions\">")
 document.write("<nav>")
 document.write("<center>")
 document.write("<ul>")
 document.write("<li>New<\/li>")
-document.write("<li><a href=»#inicio»>Get<\/a><\/li>")
-document.write("<li><a href=")
-document.write(url)
-document.write(">Modify<\/a><\/li>")
-document.write("<li><a href=»#contacto»>Push CSV<\/a><\/li>")
+document.write("<li><a href=»#get»>Get<\/a><\/li>")
+document.write("<li><a href=»#modify»>Modify<\/a><\/li>")
+document.write("<li><a href=»#csv»>Push CSV<\/a><\/li>")
 document.write("<\/ul>")
 document.write("<\/center>")
 document.write("<\/nav>")
